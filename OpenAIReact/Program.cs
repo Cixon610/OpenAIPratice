88bba01fd51d234e0768e92d11c3571d96522f01
RyanC <ryan830807@hotmail.com>
2023-05-01 23:00:43 +0800

    Add DB

--- OpenAIReact/Program.cs
+++ OpenAIReact/Program.cs
@@ -1,9 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+using OpenAIReact.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllersWithViews();
-
+builder.Services.AddDbContext<OpenAIContext>(
+    options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
