b377d4e232186a5f935d57b7b534270aaa1cd303
GAMANIA\g-ryanchang <g-ryanchang@gamania.com>
2023-05-04 15:09:03 +0800

    Add automapper

---
+++ OpenAIService/AutoMapping.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using OpenAIService.Entities;
+using OpenAIService.ViewModels;
+
+
+namespace OpenAIService
+{
+    public class AutoMapping : Profile
+    {
+        public AutoMapping()
+        {
+            //來源=>目標
+            CreateMap<User, UserVM>();
+        }
+    }
+}
---
+++ OpenAIService/ViewModels/UserVM.cs
@@ -0,0 +1,8 @@
+﻿namespace OpenAIService.ViewModels
+{
+    public class UserVM
+    {
+        public string Name { get; set; }
+        public string Account { get; set; }
+    }
+}
